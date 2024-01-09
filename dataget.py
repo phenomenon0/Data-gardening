7dbc243997ad288553ea728c9f7e0e2f5efaa27e
Femi Adeniran <66181395+phenomenon0@users.noreply.github.com>
2024-01-08 23:14:37 -0600

    stripped down commands for a new scrape

--- dataget.py
+++ dataget.py
@@ -2,27 +2,13 @@ import soccerdata as sd
 import pandas as pd
 
 #create scraper class instance 
-fbref = sd.FBref( leagues=['Big 5 European Leagues Combined','ENG-Premier League','ESP-La Liga',
- 'FRA-Ligue 1',
- 'GER-Bundesliga',
- 'ITA-Serie A'])
+fbref = sd.FBref( leagues='Big 5 European Leagues Combined')
 
 season_stats = fbref.read_team_season_stats(stat_type='shooting')
 
 
 #do the same for player stats
-player_stats = fbref.read_player_season_stats(stat_type='shooting')
-player_stats = fbref.read_player_season_stats(stat_type='passing')
-player_stats = fbref.read_player_season_stats(stat_type='passing_types')
-player_stats = fbref.read_player_season_stats(stat_type='goal_shot_creation')
-player_stats = fbref.read_player_season_stats(stat_type='defense')
-player_stats = fbref.read_player_season_stats(stat_type='possession')
-player_stats = fbref.read_player_season_stats(stat_type='playing_time')
-player_stats = fbref.read_player_season_stats(stat_type='misc')
-player_stats = fbref.read_player_season_stats(stat_type='possession')
-player_stats = fbref.read_player_season_stats(stat_type='keeper')
-player_stats = fbref.read_player_season_stats(stat_type='keeper_adv')
-
+player_stats = fbref.read_player_season_stats()
 
 
 
